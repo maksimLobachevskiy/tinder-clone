296364ab26e29ec4ac37e27c38e31c1cadb8f0a8
Lobachevskyi Maksym <lobachevskiy.maksim@gmail.com>
2022-01-21 18:38:51 +0200

    Create HerokuEnv.java

---
+++ src/main/java/com/tinder/HerokuEnv.java
@@ -0,0 +1,13 @@
+package org.avelibeyli.tinder;
+
+public class HerokuEnv {
+    public static int port(){
+        try {
+            String port = System.getenv("PORT");
+            return Integer.parseInt(port);
+        }catch (NumberFormatException ex){
+            return 5000;
+        }
+
+    }
+}
